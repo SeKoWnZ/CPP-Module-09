12eb2d0dd0e5950c8626b837d811e5ebd74fe4e0
SeKoWnZ <140755698+SeKoWnZ@users.noreply.github.com>
2025-10-21 18:11:16 +0200

    ex01 complete

--- ex01/src/RPN.cpp
+++ ex01/src/RPN.cpp
@@ -6,7 +6,67 @@
 /*   By: jose-gon <jose-gon@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 20:29:48 by jose-gon          #+#    #+#             */
-/*   Updated: 2025/10/14 20:29:49 by jose-gon         ###   ########.fr       */
+/*   Updated: 2025/10/21 18:08:57 by jose-gon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <RPN.hpp>
+
+RPN::RPN(){
+	
+}
+
+RPN::~RPN(){
+	
+}
+
+void RPN::calculate(const std::string &expression)
+{
+	std::stack<int> stack;
+	std::istringstream exp(expression);
+	std::string token;
+	
+	while (exp >> token)
+	{
+		if (token[1])
+			throw std::runtime_error("Error: wrong format.");
+		else if (isdigit(token[0]))
+			stack.push(std::atoi(token.c_str()));
+		else
+		{
+			if (stack.size() != 2)
+				throw std::runtime_error("Error: wrong number of operands.");
+
+			int b = stack.top();
+			stack.pop();
+			int a = stack.top();
+			stack.pop();
+			std::cout << "A: " << a << std::endl << "B: " << b << std::endl;
+			
+			switch (token[0])
+			{
+				case '+':
+					stack.push(a + b);
+					break;
+				case '-':
+					stack.push(a - b);
+					break;
+				case '*':
+					stack.push(a * b);
+					break;
+				case '/':
+				{
+					if (b == 0)
+						throw std::runtime_error("Error: wrong number of operands.");
+					stack.push(a / b);	
+					break;
+				}
+				default:
+					throw std::runtime_error("Error: invalid operator");
+			}
+		}
+	}
+	if (stack.size() != 1)
+		throw std::runtime_error("Error: wrong format.");
+	std::cout << "RESULT >>> " << stack.top() << " <<<" << std::endl;
+}
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,29 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jose-gon <jose-gon@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/14 20:29:31 by jose-gon          #+#    #+#             */
-/*   Updated: 2025/10/21 18:02:57 by jose-gon         ###   ########.fr       */
+/*   Created: 2025/10/21 17:42:50 by jose-gon          #+#    #+#             */
+/*   Updated: 2025/10/21 17:56:49 by jose-gon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef RPN_HPP
-#define RPN_HPP
+#include <RPN.hpp>
 
-#include <iostream>
-#include <stack>
-#include <sstream>
-#include <cstdlib>
-
-class RPN
+int main(int argc, char **argv)
 {
-	public:
-		RPN();
-		~RPN();
-		void calculate(const std::string &expresion);	
-};
+	if (argc != 2)
+	{
+		std::cerr << "YOU HAVE TO WRITE AN EXPRESSION IN REVERSE POLISH NOTATION ALONG WITH THE PROGRAM" << std::endl;
+		return 1;
+	}
+	
+	std::string expression = argv[1];
+	RPN rpn;
 
-#endif
+	try
+	{
+		rpn.calculate(expression);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
