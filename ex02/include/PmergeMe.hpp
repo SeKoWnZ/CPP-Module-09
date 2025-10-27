a491f5d0ebc444938fc853cf4d950735e3e68a5f
SeKoWnZ <140755698+SeKoWnZ@users.noreply.github.com>
2025-10-27 12:30:54 +0100

    ex02 ongoing

---
+++ ex02/include/PmergeMe.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jose-gon <jose-gon@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 10:34:36 by jose-gon          #+#    #+#             */
+/*   Updated: 2025/10/27 12:27:00 by jose-gon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+#define	PMERGEME_HPP
+
+#include <vector>
+#include <deque>
+
+class PmergeMe
+{
+	std::vector<int> _vec;
+	std::deque<int> _deque;
+
+	void mergeVec()
+}
+
+#endif
